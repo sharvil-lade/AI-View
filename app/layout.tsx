317eb0cb58208632b04141dbebb73d1e8593bdcc
sharvil-lade <sharvillade9917@gmail.com>
2025-05-14 12:40:22 +0530

    Added auth pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,34 @@
 import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
+
+
 import "./globals.css";
+import {Toaster} from "sonner";
+
+
 
 const monaSans = Mona_Sans({
-  variable: "--font-mona-sans",
-  subsets: ["latin"],
+    variable: "--font-mona-sans",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "AI View",
-  description: "An AI-powered mock interview platform",
+    title: "PrepWise",
+    description: "An AI-powered platform for preparing for mock interviews",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="dark">
-      <body
-        className={`${monaSans.className} antialiased pattern`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" className="dark">
+            <body className={`${monaSans.className} antialiased pattern`}>
+                {children}
+
+                <Toaster />
+            </body>
+        </html>
+    );
 }
